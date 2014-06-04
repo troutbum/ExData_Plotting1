945fcf9410d2ecf60dc825172b226d794fdf5009
troutbum <genec2008@gmail.com>
2014-06-03 21:06:22 -0700

    Without big files

--- plot1.R
+++ plot1.R
@@ -2,23 +2,48 @@
 # June 2014
 # Course Project 1
 
-
 # create a data subdirectory if it does not exist
 if (!file.exists("data")) {
         dir.create("data")
 }
-filePath <- "./data/"
 
-# download dataset
-fileName <- "getdata-data-GDP.csv"
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
-xFile <- paste0(filePath, fileName)
+# download file if it isn't already there
+filePath <- "./data/"
+fileZipName <- "exdata-data-household_power_consumption.zip"
+fileName <- "household_power_consumption.txt"
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+ZipFile <- paste0(filePath, fileZipName)        # path to zipped data file
+xFile <- paste0(filePath, fileName)             # path to unzipped data file
+
 if (!file.exists(xFile)) {
-        download.file(fileUrl, destfile = xFile, method ="curl")
+        download.file(fileUrl, destfile = ZipFile, method ="curl")
+        unzip(ZipFile, exdir=filePath)
         dateDownloaded <- date()
 }
-x
-#
+
+# sample data file to determine 
+
+
+sampleData <- read.csv(xFile, stringsAsFactors=FALSE,sep=";",na.strings="?", nrows = 5)
+classes <- sapply(sampleData, class)
+
+
+largeData <- read.csv("huge-file.csv", header = TRUE, colClasses = classes)
+str(largeData)
+
+
+data <- read.csv(xFile, stringsAsFactors=FALSE,sep=";",na.strings="?")
+
+classes <- sapply(data, class)
+
+as.Date(classes[1],"%d %m %Y")
+
+subdata <- data$Date=="16/12/2006"
+summary(subdata)
+
+
+
 # Big problems in Quiz 3 with mixed datatypes in columns
 # (Need to prevent creation of factor levels)
 
